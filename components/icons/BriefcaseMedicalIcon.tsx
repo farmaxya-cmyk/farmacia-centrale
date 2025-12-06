9dab285dc873ec6e315e8b6767eb943dadd78b58
farmaxya-cmyk <farmaxya@gmail.com>
2025-12-07 00:28:35 +0100

    up

--- components/icons/BriefcaseMedicalIcon.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const BriefcaseMedicalIcon = (props) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 12h.01"/><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2"/><path d="M12 18h.01"/><path d="M18 10h-2a2 2 0 0 1-2-2V4H8v4a2 2 0 0 1-2 2H2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V10Z"/><path d="M8 12h.01"/></svg>
-);
--- src/main.tsx
+++
@@ -1,1 +0,0 @@
-// This file is no longer used. The application entry point has been moved to /index.tsx.
